a121c8d094e0b6c5d8b881bd5412051e25ec95c3
jao45gg <joaosb004@gmail.com>
2024-12-22 17:41:49 -0300

    first commit

---
+++ backend/src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body } from "@nestjs/common";
+import { MessagesService } from "./messages.service";
+import { CreateMessageDto } from "./dto/create-message.dto";
+
+@Controller("messages")
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @Post()
+  create(@Body() createMessageDto: CreateMessageDto) {
+    return this.messagesService.create(createMessageDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.messagesService.findAll();
+  }
+}
---
+++ backend/src/messages/messages.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { CreateMessageDto } from "./dto/create-message.dto";
+
+@Injectable()
+export class MessagesService {
+  create(createMessageDto: CreateMessageDto) {
+    return "This action adds a new message";
+  }
+
+  findAll() {
+    return `This action returns all messages`;
+  }
+}
