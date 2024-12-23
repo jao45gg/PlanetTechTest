a297e6c6ba423d26c663c99ac49e0bc03af9c4ff
jao45gg <joaosb004@gmail.com>
2024-12-23 12:41:23 -0300

    feat/ post message dto

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup("docs", app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
--- backend/src/messages/dto/create-message.dto.ts
+++ backend/src/messages/dto/create-message.dto.ts
@@ -1,1 +1,15 @@
-export class CreateMessageDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateMessageDto {
+  @IsNotEmpty()
+  id: string;
+
+  @IsNotEmpty()
+  content: string;
+
+  @IsNotEmpty()
+  sender: string;
+
+  @IsNotEmpty()
+  timestamp: string;
+}
