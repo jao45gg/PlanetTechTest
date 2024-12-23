5b9ece2a97a1c14cc74dae90b075a75d6700b2fc
jao45gg <joaosb004@gmail.com>
2024-12-23 15:54:36 -0300

    feat/ post & get messages

--- backend/src/messages/messages.service.ts
+++ backend/src/messages/messages.service.ts
@@ -3,11 +3,34 @@ import { CreateMessageDto } from "./dto/create-message.dto";
 
 @Injectable()
 export class MessagesService {
+  db = [
+    {
+      id: "550e8400-e29b-41d4-a716-446655440000",
+      content: "Olá, como vai?",
+      sender: "João",
+      timestamp: "2024-12-23T15:32:00Z",
+    },
+    {
+      id: "9b72e4e7-44e6-43f2-901e-32a6bb69d1fa",
+      content: "Tudo ótimo, e você?",
+      sender: "Maria",
+      timestamp: "2024-12-23T15:33:00Z",
+    },
+  ];
+
+  private sortDbByTimestamp() {
+    this.db.sort(
+      (a, b) =>
+        new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime(),
+    );
+  }
+
   create(createMessageDto: CreateMessageDto) {
-    return "This action adds a new message";
+    this.db.push(createMessageDto);
+    this.sortDbByTimestamp();
   }
 
   findAll() {
-    return `This action returns all messages`;
+    return this.db;
   }
 }
